d3db3077b9541fc90396aaae98f21512e7079319
ThailanInTallinn <thailangm@icloud.com>
2024-05-21 11:11:21 +0300

    Feature: remove game loop
    The for loop that obliged player to insert 5 values, hence playing
    the game 5 times has been removed for this branch, in order for
    the UI to make more sense.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 9) + 1;
     (computerChoice >= 1 && computerChoice <= 3) ? computerChoice = "rock" :
-    (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = "paper" :
-    computerChoice = "scissors";
+        (computerChoice >= 4 && computerChoice <= 6) ? computerChoice = "paper" :
+            computerChoice = "scissors";
     return computerChoice;
 }
 
@@ -11,19 +11,19 @@ let userScore = 0;
 let computerScore = 0;
 
 let game = (player, computer) => {
-    if(player == computer) {
+    if (player == computer) {
         console.log("It's a tie!");
         tieCount++;
-    } else if(player == 'rock') {
-        if(computer == 'paper'){
+    } else if (player == 'rock') {
+        if (computer == 'paper') {
             console.log("You lose! Paper beats rock.");
             computerScore++;
-        } else if(computer == 'scissors') {
+        } else if (computer == 'scissors') {
             console.log("You won! Rock beats scissors.");
             userScore++
         }
-    } else if(player == 'paper') {
-        if(computer == 'rock') {
+    } else if (player == 'paper') {
+        if (computer == 'rock') {
             console.log("You won! Paper beats rock.");
             userScore++;
         } else if (computer == "scissors") {
@@ -31,26 +31,25 @@ let game = (player, computer) => {
             computerScore++;
         }
     } else if (player == 'scissors') {
-        if(computer == 'rock') {
+        if (computer == 'rock') {
             console.log("You lose! Rock beats Scissors");
             computerScore++;
         } else if (computer == "paper") {
             console.log("You won! Scissors beats paper.")
             userScore++;
         }
-    } 
+    }
 }
 
 function playGame() {
-    for(let i = 0; i < 5; i++){
-        const playerWeapon = prompt('Choose your weapon', 'Rock, Paper or Scissors').toLowerCase();
-        const computerWeapon = getComputerChoice();
-        game(playerWeapon, computerWeapon);
-    }
+    const playerWeapon = prompt('Choose your weapon', 'Rock, Paper or Scissors').toLowerCase();
+    const computerWeapon = getComputerChoice();
+    game(playerWeapon, computerWeapon);
+
     console.log("The score is: User " + userScore + " Computer: " + computerScore + " Ties: " + tieCount);
-    if(userScore > computerScore) {
+    if (userScore > computerScore) {
         console.log("%cYou are the fucking master of this game!", "background-color: green");
-    } else if(computerScore > userScore) {
+    } else if (computerScore > userScore) {
         console.log("%cYou suck! HAHAHAHAHAH", "background-color: red");
     } else {
         console.log("%cBoth of you suck!", "background-color: blue");
